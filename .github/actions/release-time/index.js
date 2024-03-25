d9ec27ce3005d85fb1706302ec2f0b0815101ee4
Stef Snow <stef@treasuryprime.com>
2024-03-25 12:00:31 -0700

    Make PR workflow reusable

---
+++ .github/actions/release-time/index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+
+try {
+  const now = new Date()
+  const nowHour =  Number(new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',
+    hour12: false,
+    timeZone: 'America/Los_Angeles',
+  }).format(now))
+  const nowMinutes =  Number(new Intl.DateTimeFormat('en-US', {
+    minute: 'numeric',
+    timeZone: 'America/Los_Angeles',
+  }).format(now))
+  let deployTime = 3
+  if(nowHour > 15 || nowHour < 10 && nowMinutes < 15) {
+    deployTime = 10
+  } else if(nowHour < 12 && nowMinutes < 15) {
+    nowHour = 12
+  }
+  core.setOutput("time", deployTime);
+} catch (error) {
+  core.setFailed(error.message);
+}
